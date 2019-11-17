07ace97c4c293bd788068677dee7fa996ae746e3
ElliotRedhead <elliot.redhead@gmail.com>
2019-11-17 21:32:45 +0000

    Logged into MongoDB.

--- MongoDB.py
+++ MongoDB.py
@@ -1,1 +1,9 @@
 import pymongo
+import config
+
+# myclient = pymongo.MongoClient(mongodb+srv://mycluster-ptgp4.mongodb.net/MongoDB)
+
+client = pymongo.MongoClient("mongodb+srv://"+config.username+":"+config.password+"@mycluster-ptgp4.mongodb.net/test?retryWrites=true&w=majority")
+db = client.test
+
+# mongodb+srv://Elliot:<password>@mycluster-ptgp4.mongodb.net/test?retryWrites=true&w=majority
