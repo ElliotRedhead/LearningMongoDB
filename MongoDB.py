604d27618d3bd6faaa1983a0e190416f56232349
ElliotRedhead <elliot.redhead@gmail.com>
2019-11-20 11:10:12 +0000

    Connected to database and printed collection.

--- MongoDB.py
+++ MongoDB.py
@@ -1,9 +1,23 @@
 import pymongo
 import config
 
-# myclient = pymongo.MongoClient(mongodb+srv://mycluster-ptgp4.mongodb.net/MongoDB)
+MONGODB_URI = 'mongodb+srv://'+config.username+':'+config.password+'@mycluster-ptgp4.mongodb.net/test?retryWrites=true&w=majority'
+DBS_NAME = "TestDB"
+COLLECTION_NAME = "TestMDB"
 
-client = pymongo.MongoClient("mongodb+srv://"+config.username+":"+config.password+"@mycluster-ptgp4.mongodb.net/test?retryWrites=true&w=majority")
-db = client.test
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient(url)
+        print("Mongo is connected.")
+        return conn
+    except pymongo.errors.ConnectionFailure as e:
+        print("Could not establish connection: {0}").format(e)
 
-# mongodb+srv://Elliot:<password>@mycluster-ptgp4.mongodb.net/test?retryWrites=true&w=majority
+conn = mongo_connect(MONGODB_URI)
+
+coll = conn[DBS_NAME][COLLECTION_NAME]
+
+documents = coll.find()
+
+for doc in documents:
+    print(doc)
