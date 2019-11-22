129375fab005fa5aaf22bbec87eea4108d628cfd
ElliotRedhead <elliot.redhead@gmail.com>
2019-11-22 21:32:36 +0000

    Created a user input menu.

--- mongo_project.py
+++ mongo_project.py
@@ -12,3 +12,16 @@ def mongo_connect(url):
         return conn
     except pymongo.errors.ConnectionFailure as e:
         print("Could not establish connection: {0}").format(e)
+
+def show_menu():
+    print("")
+    print("1. Add a record.")
+    print("2. Find a rcord by name.")
+    print("3. Edit a record.")
+    print("4. Delete a record.")
+    print("5. Exit.")
+
+    option = input("Enter option: ")
+    return option
+
+
