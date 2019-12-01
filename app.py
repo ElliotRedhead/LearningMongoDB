3535e68862d4d5cb952984bd8f0a02a0276dce52
ElliotRedhead <elliot.redhead@gmail.com>
2019-12-01 18:02:54 +0000

    Displayed the tasks to be completed with flask.

--- app.py
+++ app.py
@@ -1,22 +1,27 @@
 import os
-import config
+import auth
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId 
 
 app = Flask(__name__)
+MONGODB_URI = 'mongodb+srv://'+auth.username+':'+auth.password+'@mycluster-ptgp4.mongodb.net/test?retryWrites=true&w=majority'
+DBS_NAME = "task_manager"
+COLLECTION_NAME = "tasks"
 
 app.config["MONGO_DBNAME"] = "task_manager"
-app.config["MONGO_URI"] = "mongodb+srv://"+config.username+":"+config.password+"@mycluster-ptgp4.mongodb.net/test?retryWrites=true&w=majority"
+app.config["MONGO_URI"] = "mongodb+srv://"+auth.username+":"+auth.password+"@mycluster-ptgp4.mongodb.net/task_manager?retryWrites=true&w=majority"
+
 
 mongo = PyMongo(app)
+test_dict = {"insertionTest":"true"}
 
 @app.route("/")
 @app.route("/get_tasks")
 def get_tasks():
-    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+    return render_template("tasks.html", tasks=mongo.db.tasks.find(), test=mongo.db.collection.count())
 
 if __name__ == "__main__":
-    app.run(host=os.environ.get("IP"),
-        port=os.environ.get("PORT"),
+    app.run(host=os.environ.get("IP","127.0.0.1"),
+        port=os.environ.get("PORT","5000"),
         debug=True)
---
+++ test.py
@@ -0,0 +1,31 @@
+import pymongo
+import config
+
+MONGODB_URI = 'mongodb+srv://'+config.username+':'+config.password+'@mycluster-ptgp4.mongodb.net/test?retryWrites=true&w=majority'
+DBS_NAME = "task_manager"
+COLLECTION_NAME = "tasks"
+
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient(url)
+        print("Mongo is connected.")
+        return conn
+    except pymongo.errors.ConnectionFailure as e:
+        print("Could not establish connection: {0}").format(e)
+
+
+def get_record():
+    try:
+        doc = coll.find()
+        for record in doc:
+            print(record)
+    except:
+        print("Error accessing the database.")
+    
+
+conn = mongo_connect(MONGODB_URI)
+db = conn[DBS_NAME]
+coll = db[COLLECTION_NAME]
+
+get_record()
+
