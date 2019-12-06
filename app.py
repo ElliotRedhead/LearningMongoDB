06959dc387621ac326ab1897a49a2d61fef8f962
unknown <elliot.redhead@gmail.com>
2019-12-06 07:06:28 +0000

    Form iteration, categories selectable in dropdown.

--- app.py
+++ app.py
@@ -23,7 +23,8 @@ def get_tasks():
 
 @app.route("/add_task")
 def add_task():
-    return render_template("addtask.html")
+    return render_template("addtask.html",
+    categories=mongo.db.categories.find())
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP","127.0.0.1"),
