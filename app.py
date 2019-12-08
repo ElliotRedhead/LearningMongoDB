7a95c99cde1cfc70376723733bc27d9fbad5d058
ElliotRedhead <elliot.redhead@gmail.com>
2019-12-08 12:07:00 +0000

    Added ability to add new category.

--- app.py
+++ app.py
@@ -69,6 +69,10 @@ def delete_task(task_id):
 def get_categories():
     return render_template('categories.html', categories=mongo.db.categories.find())
 
+@app.route('/delete_category/<category_id>')
+def delete_category(category_id):
+    mongo.db.categories.remove({'_id': ObjectId(category_id)})
+    return redirect(url_for('get_categories'))
 
 @app.route('/edit_category/<category_id>')
 def edit_category(category_id):
@@ -78,14 +82,20 @@ def edit_category(category_id):
 def update_category(category_id):
     mongo.db.categories.update(
         {'_id': ObjectId(category_id)},
-        {'category_name': request.form.get['category_name']}
+        {'category_name': request.form.get('category_name')})
     return redirect(url_for('get_categories'))
-    )
 
-@app.route('/delete_category/<category_id>')
-def delete_category(category_id):
-    mongo.db.categories.remove({'_id': ObjectId(category_id)})
+
+@app.route('/insert_category', methods=['POST'])
+def insert_category():
+    categories = mongo.db.categories
+    category_doc = {'category_name': request.form.get('category_name')}
+    categories.insert_one(category_doc)
     return redirect(url_for('get_categories'))
+
+@app.route('/new_category')
+def new_category():
+    return render_template('addcategory.html')
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP", "127.0.0.1"),
